82f65972cd5911e37b31203e2ae63c58ee5736cb
Kihtrak <karthikandsankar@gmail.com>
2018-04-19 18:54:58 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+/*
+const navbar = document.getElementById("navbar");
+window.onscroll = function() {
+  if (window.pageYOffset >= navbar.offsetTop) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}*/
