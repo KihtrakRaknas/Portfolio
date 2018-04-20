2d62735d93495a65c0979bba2dadecb19ed5ec36
Kihtrak <karthikandsankar@gmail.com>
2018-04-19 22:14:13 -0400

    Huge Update in Content
    Added 750R logo
    added google+ profile pic
    
    added About page
    
    Added paralax effect
    
    Added JS to automatically format the page
    
    Added tabs and animations for projects

--- app.js
+++ app.js
@@ -1,3 +1,102 @@
+var logo = document.getElementById("750 logo");
+var logoText = document.getElementById("logoText");
+var tab1 = document.getElementById("tab1");
+var tab2 = document.getElementById("tab2");
+var tab3 = document.getElementById("tab3");
+var tab4 = document.getElementById("tab4");
+var content = document.getElementById("content");
+var ClickAButton = document.getElementById("ClickAButton");
+var clicked = false;
+
+ClickAButton.addEventListener("click",function(){
+    $("#ClickAButton").slideUp();
+});
+
+tab1.addEventListener("click",function(){
+    clicked = true;
+    $("#ClickAButton").slideUp();
+    $("#content1").slideDown();
+    $("#content2").slideUp();
+    $("#content3").slideUp();
+    $("#content4").slideUp();
+    tab1.classList.add("active");
+    tab2.classList.remove("active");
+    tab3.classList.remove("active");
+    tab4.classList.remove("active");
+});
+
+tab2.addEventListener("click",function(){
+    clicked = true;
+    $("#ClickAButton").slideUp();
+    $("#content1").slideUp();
+    $("#content2").slideDown();
+    $("#content3").slideUp();
+    $("#content4").slideUp();
+    tab1.classList.remove("active");
+    tab2.classList.add("active");
+    tab3.classList.remove("active");
+    tab4.classList.remove("active");
+});
+
+tab3.addEventListener("click",function(){
+    clicked = true;
+    $("#ClickAButton").slideUp();
+    $("#content1").slideUp();
+    $("#content2").slideUp();
+    $("#content3").slideDown();
+    $("#content4").slideUp();
+    tab1.classList.remove("active");
+    tab2.classList.remove("active");
+    tab3.classList.add("active");
+    tab4.classList.remove("active");
+});
+
+tab4.addEventListener("click",function(){
+    clicked = true;
+    $("#ClickAButton").slideUp();
+    $("#content1").slideUp();
+    $("#content2").slideUp();
+    $("#content3").slideUp();
+    $("#content4").slideDown();
+    tab1.classList.remove("active");
+    tab2.classList.remove("active");
+    tab3.classList.remove("active");
+    tab4.classList.add("active");
+});
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+});
+
+window.onload = function(){
+    resize();
+     var tim = setInterval(loop,100)
+};
+var num = 0;
+function loop(){
+    num++;
+    if(num==40 && !clicked)
+        $("#ClickAButton").slideDown();
+}
+
+window.onscroll = function(){
+    logoText.style.backgroundColor = "rgba(255,255,255,"+(1-(window.pageYOffset/(height/1.2)))+")";
+}
+
+window.addEventListener('resize', resize);
+
+var height;
+function resize(){
+    height = (window.innerWidth*(243/686))
+    logo.style= "min-height: "+height+"px;";
+    var borderSize = ((window.innerWidth/2)-290).toString();
+    console.log(borderSize);
+    var topBorderSize = (height-310)/2
+    console.log(topBorderSize);
+    logoText.style="margin-left: "+borderSize+"px; margin-right: "+borderSize+"px; margin-top: "+topBorderSize+"px;";
+    //logoText.style="";
+}
+
 /*
 const navbar = document.getElementById("navbar");
 window.onscroll = function() {
